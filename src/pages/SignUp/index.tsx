ab24f1264cdb7e9b68c401e46e46f2c841307b0f
Flavio Dias <flaviodias.info@gmail.com>
2020-06-28 21:59:46 -0300

    Create tooltip errors

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -25,7 +25,7 @@ const SignUp: React.FC = () => {
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'Digite no mínimo 6 dígitos'),
+        password: Yup.string().min(6, 'Mínimo 6 dígitos'),
       });
 
       await schema.validate(data, {
